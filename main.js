b02cae871e3cdb73fb131dc8099886db14ac088d
gustavoraupp45 <gustavoraupp45@gmail.com>
2025-06-09 19:22:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
 function aumentaTamanho() {
     if (tamanhoSenha < 20) {
         // tamanhoSenha = tamanhoSenha+1;
